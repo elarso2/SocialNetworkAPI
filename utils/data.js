82474ee9fea394921c63de3d1c5a01819c753906
Elizabeth Larson <elarso2@uw.edu>
2022-07-06 13:41:35 -0700

    seed data for user and thought

---
+++ utils/data.js
@@ -0,0 +1,31 @@
+const users = [
+  {
+    username: "tedMosby",
+    email: "mosbius@gmail.com",
+  },
+  {
+    username: "barneyStintson",
+    email: "legendary@gmail.com",
+  },
+  {
+    username: "robinSherb",
+    email: "gocanada@gmail.com",
+  },
+];
+
+const thoughts = [
+  {
+    thoughtText:
+      "Someone asked me where I live...I told them my parents live in Chicago..but I live in the moment",
+    username: "tedMosby",
+  },
+  {
+    thoughtText: "Get a girl without speaking to her?? Challenge accepted!!",
+    username: "barneyStintson",
+  },
+  {
+    thoughtText:
+      "If Barney tries to disrepect Canada again, I'm going to go all Hoser on him!",
+    username: "robinSherb",
+  },
+];
