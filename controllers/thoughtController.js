a843ea6cf3e13ec1be161691653f23d6bcd0dbf4
Elizabeth Larson <elarso2@uw.edu>
2022-07-07 00:23:44 -0700

    get thoughts fxn

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,11 @@ const { User, Thought } = require("../models");
 
 module.exports = {
   // get all thoughts
-  getThoughts() {},
+  getThoughts(req, res) {
+    Thought.find().then((thoughts) =>
+      res.json(thoughts).catch((err) => res.status(500).json(err))
+    );
+  },
 
   // get thought by id
   getThoughtById() {},
